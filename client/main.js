14e13727e642a6aae70afb62b99b09aa93de4e91
skawls <ventu9204@gmail.com>
2023-07-03 14:20:42 +0900

    first commit

---
+++ client/main.js
@@ -0,0 +1,6 @@
+console.log('hello js!');
+
+const a = 10;
+
+const b = 10;
+
---
+++ server/index.js
@@ -0,0 +1,11 @@
+
+const liverServer = require('live-server')
+
+const params = {
+port:5500,
+host:'localhost',
+root:'/.client',
+open:false
+}
+
+liverServer.start(params);
